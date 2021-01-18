61ac924c22dacf1efd531033a029886f6f741ce6
Brandon Haley <kyle7286@gmail.com>
2021-01-17 21:04:25 -0500

    figured out past, present and future color change logic, basd on current hour set to run every second

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,14 +2,51 @@
 var dateTime = luxon.DateTime; //Grabbing base time object
 var localTime = dateTime.local();
 
-console.log(localTime);
-
 // Assign current day to title
 $("#currentDay").text(formatTimeTitle(localTime))
 
 // Create HTML Elements
-createRows();
+createRows(localTime.hour);
 
+
+
+// Set a timer to check the hour every second and set the colors accordingly
+var j = 9;
+var myVar = setInterval(function () {
+    console.log(localTime.hour);
+
+
+    setPastPresentFuture(j)
+    if (j === 17) { j = 8 }
+    j++
+
+}, 1000);
+
+
+// When save button is clicked, loop thru each textAreas, save to storage
+
+
+
+
+// Loop thru all text boxes and set color based on current hour
+function setPastPresentFuture(hour) {
+    console.log("New second...");
+    $('.description').each(function () {
+        var row = $(this)
+        var rowVal = parseInt($(this).attr("value"));
+
+        if (rowVal === hour) {
+            row.attr("class", "col-10 description present");
+        } else if (rowVal > hour) {
+            row.attr("class", "col-10 description future");
+        } else if (rowVal < hour) {
+            row.attr("class", "col-10 description past");
+        }
+
+    });
+
+
+}
 
 
 // Format time to unique format specific to this website
@@ -19,7 +56,7 @@ function formatTimeTitle(time) {
     var wd = time.weekday
     var tg;
     var timeTitle;
-    
+
     if (wd === "1" || wd === "21" || wd === "31") {
         tg = "st";
     } else if (wd === "2" || wd === "22") {
@@ -34,9 +71,11 @@ function formatTimeTitle(time) {
 }
 
 
-function createRows() {
+function createRows(hour) {
     // Grab the container
     var container = $(".container");
+    var icon = '<i class="fas fa-save"></i>'
+
 
     // Set some variables to help dynamically create elements
     var hourIndex = 9;
@@ -46,8 +85,8 @@ function createRows() {
         // Create the elements you'll need and give them attribute classes & id's
         var row = $("<div>").attr("class", "row").attr("id", "h" + i);
         var col1 = $("<div>").attr("class", "col-1 hour").text(hourIndex + amPm);
-        var col2 = $("<textarea>").attr("class", "col-10 description past").attr("id", "d" + i);
-        var col3 = $("<button>").attr("class", "col-1 saveBtn").text("Save");
+        var col2 = $("<textarea>").attr("class", "col-10 description past").attr({ id: "d" + i, value: i });
+        var col3 = $("<button>").attr("class", "col-1 saveBtn").html(icon);
 
         // Append columns to row
         row.append(col1, col2, col3);
